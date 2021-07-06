0ff97b648bb65c268c0687d130f285d6b414ad53
Prabal2207 <prabal760@gmail.com>
2021-07-06 22:11:44 +0530

    [Prabal] Added:-Player checks for option

--- SnakeandLadderProgram/Program.cs
+++ SnakeandLadderProgram/Program.cs
@@ -15,10 +15,41 @@ namespace SnakeandLadderProgram
         public static void SnakeLadder()
         {
             int pos1 = 0;
+            int Play = 1;
+            int Snake = 2;
+            int max = 100;
+            int min = 0;
+
             Random random = new Random();
             int player1 = random.Next(1, 7);
-            Console.WriteLine(player1);
+            int option1 = random.Next(1, 3);
+            if (pos1 == min && player1 != 6)
+            {
+                
+                Console.WriteLine("Position of player1:" + pos1);
+            }
+
+            else if (option1 == Play)
+            {
+                
+                pos1 += player1;
+                Console.WriteLine("Position of player1:" + pos1);
+
+
+
+            }
+
+            else if (option1 == Snake)
+            {
+                
+                pos1 -= player1;
+                Console.WriteLine("Position of player1:" + pos1);
+
+            }
+
+
         }
-       
+
     }
 }
+ 
