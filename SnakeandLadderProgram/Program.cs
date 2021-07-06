4568e97135445b062d784973cc905fb26077ca98
Prabal2207 <prabal760@gmail.com>
2021-07-06 23:20:25 +0530

    [Prabal] Added:-Number of times dice roll is calculated

--- SnakeandLadderProgram/Program.cs
+++ SnakeandLadderProgram/Program.cs
@@ -15,28 +15,38 @@ namespace SnakeandLadderProgram
         public static void SnakeLadder()
         {
             int pos1 = 0;
-            int Play = 1;
-            int Snake = 2;
             int max = 100;
             int min = 0;
 
             Random random = new Random();
-            int player1 = random.Next(1, 7);
-            int option1 = random.Next(1, 3);
+
+            int Play = 1;
+            int Snake = 2;
+
+            int Num_of_Times_Diceroll = 0;
 
             while (pos1 < max)
             {
 
+                int player1 = random.Next(1, 7);
+
+                int player2 = random.Next(1, 7);
+
+                int option1 = random.Next(1, 3);
+                int option2 = random.Next(1, 3);
+
+                //for player 1
+
                 if (pos1 == min && player1 != 6)
                 {
-                    
+                    Num_of_Times_Diceroll++;
                     Console.WriteLine("Position of player1:" + pos1);
                 }
 
                 else if (option1 == Play)
                 {
-                    
-                    pos1 =pos1+ player1;
+                    Num_of_Times_Diceroll++;
+                    pos1 += player1;
 
 
                     if (pos1 > max)
@@ -49,8 +59,8 @@ namespace SnakeandLadderProgram
 
                 else if (option1 == Snake)
                 {
-                   
-                    pos1 =pos1- player1;
+                    Num_of_Times_Diceroll++;
+                    pos1 -= player1;
                     if (pos1 < min)
                     {
                         pos1 = 0;
@@ -59,10 +69,9 @@ namespace SnakeandLadderProgram
                     Console.WriteLine("Position of player1:" + pos1);
                 }
 
-
-
-
             }
+            Console.WriteLine("Number of Dice Roll=" + Num_of_Times_Diceroll);
+
 
 
         }
